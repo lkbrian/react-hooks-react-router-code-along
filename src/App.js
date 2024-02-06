19c2ee442be491ef7cef28d31ac5ebaf2390d0e7
lkbrian <lkbrian742@gmail.com>
2024-02-06 19:48:56 +0300

    completed assignment

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
